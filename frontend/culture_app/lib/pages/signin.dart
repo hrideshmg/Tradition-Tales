278fcf66f17c15e162e2d55801da532ff2626a75
ItsAbhinavM <abhinav051017@gmail.com>
2024-04-20 18:32:47 +0530

    wip: finished sign in UI

--- frontend/culture_app/lib/pages/signin.dart
+++ frontend/culture_app/lib/pages/signin.dart
@@ -9,23 +9,33 @@ class SignIn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Container(
-        color: null,
-        child:  Padding(
+      body:Stack(
+
+        // decoration:const BoxDecoration(
+        //   image: DecorationImage(image: AssetImage("assets/images/Totem.jpg"),fit: BoxFit.cover),
+        //   gradient: RadialGradient(center: Alignment.center,radius: 1.0,colors: <Color>[Colors.blue,Colors.white],stops: [0.1,1.0]),
+        // ),
+        children:  [
+          Image.asset("assets/images/pooram.jpeg",fit: BoxFit.cover,height: double.infinity,),
+          Container(decoration: BoxDecoration(
+            gradient: LinearGradient(begin: Alignment.topCenter,end: Alignment.bottomCenter,colors: <Color>[Colors.transparent,Color.fromARGB(255, 0, 0, 0)],stops: [0.25,0.9])
+          ),),
+          Padding(
           padding:const EdgeInsets.all(25.0),
           child:  SingleChildScrollView(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Image.asset("name"),
               const  SizedBox(height: 495,),
               const SizedBox(height: 18,),
               TextField(
                 style:const TextStyle(color: Colors.white),
                 decoration: InputDecoration(
+                  fillColor: Colors.white,
                   hintText: "username",
+                  hintStyle: TextStyle(color: Colors.white),
                   border: OutlineInputBorder(borderRadius:BorderRadius.circular(12)),
-                  suffixIcon:const Icon(Icons.person,color: Colors.black,)
+                  suffixIcon:const Icon(Icons.person,color: Colors.white,)
                 ),
               ),
               const SizedBox(height: 18,),
@@ -33,13 +43,14 @@ class SignIn extends StatelessWidget {
                 style:const TextStyle(color: Colors.white),
                 decoration: InputDecoration(
                   hintText: "password",
+                  hintStyle:const TextStyle(color: Colors.white),
                   border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
-                  suffixIcon: const Icon(Icons.password,color: Colors.black,)
+                  suffixIcon: const Icon(Icons.password,color: Colors.white,)
                 ),
               ),const SizedBox(height: 22,),
               Row(
                 children: [
-                  Text("Don't have an account?",style: TextStyle(color: Colors.black)),
+                  Text("Don't have an account?",style: TextStyle(color: Colors.white)),
                   SizedBox(width: 9,),
                   GestureDetector(
                     onTap: null,
@@ -53,7 +64,7 @@ class SignIn extends StatelessWidget {
                 minimumSize: MaterialStateProperty.all(const Size(double.infinity,50)),backgroundColor:MaterialStateProperty.all(Color(0xff4782ba)) )),
             ],),
           ),
-        ),
+        )],
       ),
     );
   }
