9e63c1c214be0388e94a84f14356eefdc974bb96
ItsAbhinavM <abhinav051017@gmail.com>
2024-04-20 17:24:47 +0530

    wip:SignIn page

--- frontend/culture_app/lib/pages/signin.dart
+++ frontend/culture_app/lib/pages/signin.dart
@@ -1,49 +1,60 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
-
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  late String username = UserNameController.text;
-
-  final storage = const FlutterSecureStorage();
-  final loginUrl = "127.0.0.1:800";
-
-  TextEditingController UserNameController = TextEditingController();
-  TextEditingController PasswordController = TextEditingController();
-
-  Future<dynamic> sendPostRequest() async {
-    var response = await http.post(Uri.parse(loginUrl),
-        headers: {"Content-Type": "application/json"},
-        body: jsonEncode({
-          "username": UserNameController.text,
-          "password": PasswordController.text,
-        }));
-
-    if (response.statusCode == 200) {
-      String token = jsonDecode(response.body)["token"];
-
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-        content: Text("Sign in Succesfull"),
-      ));
-      await storage.write(key: 'token', value: token);
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-        content: Text("Sign in failed, please try again"),
-      ));
-    }
-    return response;
-  }
+class SignIn extends StatelessWidget {
+  const SignIn({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body:Container(
+        color: null,
+        child:  Padding(
+          padding:const EdgeInsets.all(25.0),
+          child:  SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset("name"),
+              const  SizedBox(height: 495,),
+              const SizedBox(height: 18,),
+              TextField(
+                style:const TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  hintText: "username",
+                  border: OutlineInputBorder(borderRadius:BorderRadius.circular(12)),
+                  suffixIcon:const Icon(Icons.person,color: Colors.black,)
+                ),
+              ),
+              const SizedBox(height: 18,),
+              TextField(
+                style:const TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  hintText: "password",
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+                  suffixIcon: const Icon(Icons.password,color: Colors.black,)
+                ),
+              ),const SizedBox(height: 22,),
+              Row(
+                children: [
+                  Text("Don't have an account?",style: TextStyle(color: Colors.black)),
+                  SizedBox(width: 9,),
+                  GestureDetector(
+                    onTap: null,
+                    child:const Text("Sign-up",style: TextStyle(color: Color(0xff4782ba)),),
+                  )
+                ],
+              )
+              ,const SizedBox(height: 75,),
+              ElevatedButton(onPressed: null,child: Text("Sign-In",style: TextStyle(fontSize:18,color: Colors.white ),),style: ButtonStyle(
+                shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))),
+                minimumSize: MaterialStateProperty.all(const Size(double.infinity,50)),backgroundColor:MaterialStateProperty.all(Color(0xff4782ba)) )),
+            ],),
+          ),
+        ),
+      ),
+    );
   }
 }
