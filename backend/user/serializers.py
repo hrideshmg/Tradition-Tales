1aaf716e8a1778ec65d5b0f36234d1c8ba35ea38
hridesh <hridesh699@gmail.com>
2024-04-21 01:01:09 +0530

    fixed backend settings

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -1,8 +1,7 @@
-from rest_framework import serializers
 from django.contrib.auth.models import User
-from rest_framework.exceptions import ValidationError
-from rest_framework import status
+from rest_framework import serializers, status
 from rest_framework.authtoken.models import Token
+from rest_framework.exceptions import ValidationError
 
 
 class UserLoginSerializer(serializers.ModelSerializer):
@@ -18,34 +17,28 @@ class UserLoginSerializer(serializers.ModelSerializer):
 class UserRegisterSerializer(serializers.ModelSerializer):
     id = serializers.PrimaryKeyRelatedField(read_only=True)
     username = serializers.CharField()
-    email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ["id", "username","email", "password"]
-        extra_kwargs = {
-            'password': {"write_only": True}
-        }
+        fields = ["id", "username", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
 
     def validate_username(self, username):
         if User.objects.filter(username=username).exists():
-            detail = {
-                "detail": "User Already exist!"
-            }
+            detail = {"detail": "User Already exist!"}
             raise ValidationError(detail=detail)
         return username
 
     def validate(self, instance):
-        if User.objects.filter(email=instance['email']).exists():
-            raise ValidationError({"message": "Email already taken!"})
 
         return instance
 
     def create(self, validated_data):
-        passowrd = validated_data.pop('password')
+        passowrd = validated_data.pop("password")
         user = User.objects.create(**validated_data)
         user.set_password(passowrd)
         user.save()
         Token.objects.create(user=user)
         return user
+
